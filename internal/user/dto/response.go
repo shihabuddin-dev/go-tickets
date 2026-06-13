fce5350afdd4071274032ef012e1cca35aad890f
shihabuddin-dev <shihabuddin.dev@gmail.com>
2026-06-13 09:25:38 +0600

    implement user model and DTOs for create and response requests

---
+++ internal/user/dto/response.go
@@ -0,0 +1,8 @@
+package dto
+
+type Response struct {
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"created_at"`
+}
